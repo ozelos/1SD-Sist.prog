6fddf8321e5cef756fb46cc512cbbbe647fc8b2f
ozelos <99196325+ozelos@users.noreply.github.com>
2024-10-30 21:19:30 +0200

    student.cpp v1.0

--- student.cpp
+++ student.cpp
@@ -1,19 +1,9 @@
 #include "studentas.h"
-#include <numeric>
 #include <iomanip>
 
-Studentas::Studentas(const std::string& vardas, const std::string& pavarde, const std::vector<int>& namuDarbai, int egzaminas)
-    : vardas_(vardas), pavarde_(pavarde), namuDarbai_(namuDarbai), egzaminas_(egzaminas) {
-    skaiciuotiGalutini();
-}
-
-void Studentas::skaiciuotiGalutini() {
-    double vidurkis = std::accumulate(namuDarbai_.begin(), namuDarbai_.end(), 0.0) / namuDarbai_.size();
-    galutinis_ = 0.4 * vidurkis + 0.6 * egzaminas_;
-}
-
 std::ostream& operator<<(std::ostream& out, const Studentas& studentas) {
-    out << std::setw(12) << studentas.pavarde_ << std::setw(12) << studentas.vardas_
-        << std::setw(10) << std::fixed << std::setprecision(2) << studentas.galutinis_;
+    out << studentas.vardas_ << " " << studentas.pavarde_ 
+        << " Galutinis: " << std::fixed << std::setprecision(2) 
+        << studentas.galutinis_;
     return out;
 }
